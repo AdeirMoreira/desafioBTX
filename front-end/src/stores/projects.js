d11b60fe83b8d208ecf3d26cae44c0e2e0f3dd30
AdeirMoreira <adeir.maia@hotmail.com>
2022-11-24 23:45:34 -0300

    feat: add ordenação de projetos e tasks

--- front-end/src/stores/projects.js
+++ front-end/src/stores/projects.js
@@ -12,19 +12,36 @@ export const useProjectStore = defineStore("project", {
 	actions: {
 		setAllProjects(prjects) {
 			this.projects = prjects;
+			this.sortProjects();
 		},
 		addProject(project) {
 			this.projects.push(project);
 		},
 		removeProject(id) {
 			this.projects = this.projects.filter((project) => project.id !== id);
-			this.setDeletedProjectId(id)
+			this.setDeletedProjectId(id);
+			this.sortProjects();
 		},
-        setProjectSelected(id){
-            this.projectSelectedId = id
-        },
-		setDeletedProjectId(id){
-            this.deletedProjectId = id
-        }
+		setProjectSelected(id) {
+			this.projectSelectedId = id;
+		},
+		setDeletedProjectId(id) {
+			this.deletedProjectId = id;
+		},
+		sortProjects() {
+			this.projects = this.projects.sort((a, b) => {
+				const atime = new Date(
+					`${a.createdAt.split(" ")[0].split("/").reverse().join("/")} ${
+						a.createdAt.split(" ")[1]
+					}`
+				).getTime();
+				const btime = new Date(
+					`${b.createdAt.split(" ")[0].split("/").reverse().join("/")} ${
+						b.createdAt.split(" ")[1]
+					}`
+				).getTime();
+				return atime - btime;
+			});
+		},
 	},
 });
--- front-end/src/stores/tasks.js
+++ front-end/src/stores/tasks.js
@@ -10,21 +10,23 @@ export const useTasksStore = defineStore("task", {
 	actions: {
 		setTasks(tasks) {
 			this.tasks = tasks;
+			this.sortTasks()
 		},
 		addTask(task) {
 			this.tasks.push(task);
-		},
-		editTask(taskEdited) {
-			const newTask = this.tasks.map((task) =>
-				task.id === taskEdited.id ? taskEdited : task
-			);
-			console.log(newTask);
+			this.sortTasks()
 		},
 		removeTask(id) {
 			this.tasks = this.tasks.filter((task) => task.id !== id);
+			this.sortTasks()
 		},
 		removeAllTasksByProject(id){
 			this.tasks = this.tasks.filter(task => task.projectId !== id)
+		},
+		sortTasks(){
+			this.tasks = this.tasks.sort((a,b) => {
+				return new Date(a.deadLine).getTime() - new Date(b.deadLine)
+			})
 		}
 	},
 });
